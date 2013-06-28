6533d5f59232db7eada57fc2d9c86f5518563083
schinken <schinken@hackerspace-bamberg.de>
2013-06-28 17:14:59 +0200

    using status_api directly

--- main.js
+++ main.js
@@ -2,11 +2,14 @@ var Udpio = require('./Udpio')
    ,Door  = require('./components/Door')
    ,Heater = require('./components/Heater');
 
+var status_api = new StatusAPI(settings.status_api, 120);
+
 var doorcontrol = new Door();
 var heater = new Heater();
 
 var udp_events = new Udpio('AIO0');
 
+// Doorcontrol
 udp_events.on('doorlock', function(val) {
     doorcontrol.lock(val);
 });
@@ -17,4 +20,13 @@ udp_events.on('doorframe', function(val){
 
 udp_events.on('doorbutton', function(val){
     doorcontrol.button(val);
+});
+
+// Heater
+status_api.on('space_closed', function() {
+    header.switch_off();
+});
+
+status_api.on('space_opened', function() {
+    heater.switch_on();
 });
