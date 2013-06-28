636baf89b03987c0489eca4cb3bd61729c2f23f0
schinken <schinken@hackerspace-bamberg.de>
2013-06-28 12:07:52 +0200

    moved modules to components

---
+++ main.js
@@ -0,0 +1,17 @@
+var Udpio = require('udpio'),
+    Door  = require('./modules/Door');
+
+var doorcontrol = new Door();
+var udp_events = new Udpio('AIO0');
+
+udp_events.on('doorlock', function(val) {
+    doorcontrol.lock(val);
+});
+
+udp_events.on('doorframe', function(val){
+    doorcontrol.frame(val);
+});
+
+udp_events.on('doorbutton', function(val){
+    doorcontrol.button(val);
+});
