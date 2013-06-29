4505004be48f4ca42b305c9a58fd77411a01dbb0
schinken <schinken@hackerspace-bamberg.de>
2013-06-29 18:52:39 +0200

    added database example credentials

--- settings.example.js
+++ settings.example.js
@@ -4,6 +4,7 @@ module.exports = {
        'path': '/verify_pw',
        'pass': ''
     },
+
     'snmp_host': '10.1.20.1',
     'relais_host': 'https://webrelais.bckspc.de',
     'status_api': 'http://status.bckspc.de/status.php?response=json',
@@ -11,4 +12,11 @@ module.exports = {
         'notleuchte_weiss': 3,
         'heizung':          6
     },
+    'db': {
+        'host'     : 'violet',
+        'database' : 'spaceportal',
+        'user'     : 'contactors',
+        'password' : ''
+    }
+
 };
