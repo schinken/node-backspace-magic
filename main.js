e080af21767112edee19448747416e45a8f89b9b
schinken <schinken@hackerspace-bamberg.de>
2013-06-29 18:57:34 +0200

    added logging events

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var Udpio = require('./Udpio')
    ,Door  = require('./components/Door')
    ,Heater = require('./components/Heater')
+   ,DatabaseLog = require('./components/DatabaseLog')
    ,winston = require('winston')
    ,settings = require('./settings')
    ,StatusAPI = require('bckspc-status');
@@ -18,6 +19,7 @@ var status_api = new StatusAPI(settings.status_api, 120);
 
 var doorcontrol = new Door(logger);
 var heater = new Heater(logger);
+var dblog = new DatabaseLog(logger);
 
 var udp_events = new Udpio('AIO0', settings.udpio.port, logger);
 
@@ -27,14 +29,17 @@ udp_events.init();
 // Doorcontrol
 udp_events.on('doorlock', function(val) {
     doorcontrol.lock(val);
+    dblog.logEvent('SCHLOSS', val);
 });
 
 udp_events.on('doorframe', function(val){
     doorcontrol.frame(val);
+    dblog.logEvent('RAHMEN', val);
 });
 
 udp_events.on('doorbutton', function(val){
     doorcontrol.button(val);
+    dblog.logEvent('TASTER', val);
 });
 
 // Heater
