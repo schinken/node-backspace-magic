3f24f75fe831da62d006788e868d416abaaff62e
schinken <schinken@hackerspace-bamberg.de>
2013-06-29 18:51:54 +0200

    added database logging

---
+++ components/DatabaseLog.js
@@ -0,0 +1,66 @@
+var settings = require('../settings');
+
+var DatabaseLog = function(logger) {
+    this.logger = logger;
+    this.connection = this.connect();
+};
+
+DatabaseLog.prototype.handleDisconnect(connection) {
+
+    var that = this;
+
+    connection.on('error', function(err) {
+        if (!err.fatal) {
+            return;
+        }
+
+        if (err.code !== 'PROTOCOL_CONNECTION_LOST') {
+            throw err;
+        }
+
+        that.logger.error('Re-connecting lost connection: ' + err.stack);
+        that.connection = that.connect();
+    });
+}
+
+DatabaseLog.prototype.connect = function() {
+
+    this.logger.info('Establishing connection to '+settings.db.host);
+
+    var connection = mysql.createConnection({
+        host:       settings.db.host,
+        database:   settings.db.database,
+        user:       settings.db.user,
+        password:   settings.db.password
+    });
+
+    this.handleDisconnect(connection);
+    connection.connect();
+
+    return connection;
+};
+
+handleDisconnect(connection);
+
+DatabaseLog.prototype.onEvent = function(contact, value) {
+
+    var log = this.logger;
+
+    log.info('Logging contact '+contact);
+
+    var query = this.connection.query('INSERT INTO contactors SET ?', {
+        'contact': contact,
+        'status': val,
+        'erfda': new Date()
+    },
+    function(err, result) {
+        if(err) {
+            log.error('Logging contact '+contact+' failed');
+            return false;
+        }
+
+        log.info('Logged contact '+contact+' sucessfully');
+    });
+};
+
+module.exports = DatabaseLog;
