8b7033a701131e174e1366998066a66c4f483870
schinken <schinken@hackerspace-bamberg.de>
2013-06-30 17:11:34 +0200

    updated settings

--- settings.example.js
+++ settings.example.js
@@ -1,15 +1,17 @@
 module.exports = {
    'door': {
        'host': 'door.bckspc.de',
-       'path': '/verify_pw',
+       'path': '/verify',
        'pass': ''
     },
-
-    'snmp_host': '10.1.20.1',
+    'udpio': {
+        'port': 5042  
+    },
     'relais_host': 'https://webrelais.bckspc.de',
     'status_api': 'http://status.bckspc.de/status.php?response=json',
     'relais': {
         'notleuchte_weiss': 3,
+        'notleuchte_weiss': 4,
         'heizung':          6
     },
     'db': {
