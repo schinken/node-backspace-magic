1c4e582ab4148ce78d3f9797d5a6737d83a1cddf
schinken <schinken@hackerspace-bamberg.de>
2014-07-27 14:59:59 +0200

    Moved doorbutton to OpenHAB; Only logging left

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 var Udpio = require('./Udpio')
-   ,Door  = require('./components/Door')
    ,DatabaseLog = require('./components/DatabaseLog')
    ,winston = require('winston')
    ,settings = require('./settings')
@@ -20,8 +19,6 @@ logger.add(winston.transports.Console, {
 
 var status_api = new StatusAPI(settings.status_api, 120);
 
-var doorcontrol = new Door(logger);
-
 var dblog = new DatabaseLog(logger);
 
 var udp_events = new Udpio('AIO0', settings.udpio.port, settings.udpio.ip, logger);
@@ -31,17 +28,14 @@ udp_events.init();
 
 // Doorcontrol
 udp_events.on('doorlock', function(val) {
-    doorcontrol.lock(val);
     dblog.logEvent('SCHLOSS', val);
 });
 
 udp_events.on('doorframe', function(val){
-    doorcontrol.frame(val);
     dblog.logEvent('RAHMEN', val);
 });
 
 udp_events.on('doorbutton', function(val){
-    doorcontrol.button(val);
     dblog.logEvent('TASTER', val);
 });
 
